9e6f0c7d55db1c59da804eccf0fdcdf842112cea
SoloviovO <soloviov.o.m@gmail.com>
2023-01-31 17:45:11 +0200

    add hbs render

--- src/index.js
+++ src/index.js
@@ -2,27 +2,19 @@ import './css/styles.css';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import ApiService from './fetchImages';
 import throttle from 'lodash.throttle';
-import { renderCardsOfImages } from './renderMarkup';
-
-const form = document.querySelector('#search-form');
-export const gallery = document.querySelector('.gallery');
-const loadMore = document.querySelector('.load-more');
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import renderCards from './templates/render-card.hbs';
+
+const form = document.querySelector('#search-form');
+const gallery = document.querySelector('.gallery');
 
 const apiService = new ApiService();
 
 form.addEventListener('submit', onSubmitForm);
-// Закоментована функціональність кнопки Завантажити більше
-// Для ввімкнення даної можливості слід розкоментувати всі коментарі
-// і закоментувати блок Обробка при скролі
-
-// loadMore.addEventListener('click', onLoadMoreBtnClick);
-btnHidden();
 
 function onSubmitForm(event) {
   event.preventDefault();
-  btnHidden();
   apiService.query = event.target.elements.searchQuery.value;
 
   if (apiService.query === '') {
@@ -31,52 +23,17 @@ function onSubmitForm(event) {
     );
   }
   apiService.resetPage();
+
   apiService.fetchImages().then(photos => {
-    // if (photos.totalHits > 40) {
-    //   btnVisualy();
-    // } else {
-    //   btnHidden();
-    // }
     Notify.info(`Hooray! We found ${photos.totalHits} images.`);
     clearGallery();
-    renderCardsOfImages(photos.hits);
+    gallery.insertAdjacentHTML('beforeend', renderCards(photos.hits));
     const lightbox = new SimpleLightbox('.gallery a', {
       captionsData: 'alt',
       captionDelay: 250,
     }).refresh();
   });
 }
-function clearGallery() {
-  gallery.innerHTML = '';
-}
-
-function btnHidden() {
-  loadMore.classList.add('is-hidden');
-}
-
-function btnVisualy() {
-  loadMore.classList.remove('is-hidden');
-}
-
-// function onLoadMoreBtnClick() {
-//   apiService.fetchImages().then(photos => {
-//     if (photos.totalHits / 40 > apiService.page) {
-//       btnVisualy();
-//     } else {
-//       Notify.failure(
-//         "We're sorry, but you've reached the end of search results."
-//       );
-//       btnHidden();
-//     }
-//     renderCardsOfImages(photos.hits);
-//     const lightbox = new SimpleLightbox('.gallery a', {
-//       captionsData: 'alt',
-//       captionDelay: 250,
-//     }).refresh();
-//   });
-// }
-
-// =================== Обробка при скролі =============================
 
 function onLoadMoreScroll() {
   apiService.fetchImages().then(photos => {
@@ -85,13 +42,14 @@ function onLoadMoreScroll() {
         "We're sorry, but you've reached the end of search results."
       );
     }
-    renderCardsOfImages(photos.hits);
+    gallery.insertAdjacentHTML('beforeend', renderCards(photos.hits));
     const lightbox = new SimpleLightbox('.gallery a', {
       captionsData: 'alt',
       captionDelay: 250,
     }).refresh();
   });
 }
+
 window.addEventListener('scroll', throttle(onScrollCards, 100));
 
 function onScrollCards() {
@@ -99,4 +57,8 @@ function onScrollCards() {
   if (documentRect.bottom < document.documentElement.clientHeight + 150) {
     onLoadMoreScroll();
   }
+}
+
+function clearGallery() {
+  gallery.innerHTML = '';
 }
